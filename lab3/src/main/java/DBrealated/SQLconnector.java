bb2d3a81316e5c617aa9a80a37584bdab33589d5
спс никитос <163382818+semyonov1511@users.noreply.github.com>
2024-05-23 01:35:33 +0300

    Reading layout with try-with-resources added

--- lab3/src/main/java/DBrealated/SQLconnector.java
+++ lab3/src/main/java/DBrealated/SQLconnector.java
@@ -1,11 +1,21 @@
 package DBrealated;
 
+import ReactorsRelated.Reactor;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class SQLconnector {
-    
+
+    Connection connection;
+
+    public void setConnection() {
+        this.connection = connectToDatabase();
+    }
+
     public Connection connectToDatabase() {
         Connection connection = null;
         try {
@@ -29,6 +39,24 @@ public class SQLconnector {
         }
         return connection;
     }
-    
-    public void readDatabase(ArrayList<)
+
+    public ArrayList<Reactor> readDataBase(ArrayList<Reactor> reactorTypes) {
+        ArrayList<Reactor> reactors = new ArrayList<>();
+        try (PreparedStatement preparedStatement = connection.prepareStatement("""
+                    SELECT reactor.id, reactor_name, thermal_capacity, country_name, region_name, type_name, operator_name, owner_name
+                    FROM reactor
+                    LEFT JOIN country ON country_id = country.id
+                    LEFT JOIN region ON region_id = region.id
+                    LEFT JOIN type ON type_id = type.id
+                    LEFT JOIN operator ON operator_id = operator.id
+                    LEFT JOIN owner ON owner_id = owner.id"""); 
+                ResultSet resultSet = preparedStatement.executeQuery()) {
+            while (resultSet.next()) {
+            }
+            System.out.println("БД успешно прочитана");
+        } catch (SQLException e) {
+            System.out.println("Oшибка при чтении из БД " + e.getMessage());
+        }
+        return reactors;
+    }
 }
--- lab3/src/main/java/lab4/lab4.java
+++ lab3/src/main/java/lab4/lab4.java
@@ -5,6 +5,6 @@ public class lab4 {
 
     public static void main(String[] args) {
         DBManager manager = new DBManager();
-        manager.setConnection();
+        //manager.setConnection();
     }
 }
